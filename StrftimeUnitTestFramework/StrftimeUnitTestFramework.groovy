431a9cf7233756ebc8de5714c5a660ddee775d2c
mike_neck <jkrt3333@gmail.com>
2012-01-04 02:33:13 +0900

    add console.log style test

---
+++ StrftimeUnitTestFramework/StrftimeUnitTestFramework.groovy
@@ -0,0 +1,90 @@
+class ConstantPath {
+    static final String serverPath = 'C:/Users/mike/repo/TestDrivenJavascriptDevelopment/StrftimeUnitTestFramework/'
+}
+
+@Grab('org.eclipse.jetty:jetty-server:7.5.4.v20111024')
+@Grab('commons-logging:commons-logging:1.1.1')
+
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.handler.AbstractHandler
+import org.eclipse.jetty.server.Request
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import groovy.xml.MarkupBuilder
+
+def inet = new InetSocketAddress('localhost', 3309)
+def server = new Server(inet)
+
+class JsUnitTestServer extends AbstractHandler {
+
+    def definePath = {
+        def path = new File('strftime.html').getAbsolutePath().replace('strftime.html', '')
+        if (path.contains('StrftimeUnitTestFramework') == false) {
+            path = ConstantPath.getServerPath()
+        }
+        path
+    }
+    
+    def path
+
+    def js = [:]
+
+    JsUnitTestServer() {
+        super()
+        this.path = definePath()
+        js << ['/js/strftime.js' : new File("${path}js/strftime.js")]
+        js << ['/js/strftime_test.js' : new File("${path}js/strftime_test.js")]
+    }
+
+    @Override
+    public void handle(
+            String target,
+            Request baseRequest,
+            HttpServletRequest req,
+            HttpServletResponse res) {
+        println target
+        if('/'.equals(target)) {
+            res.setStatus(200)
+            res.setContentType('text/html')
+            def w = res.writer
+            new File("${path}strftime.html").eachLine {
+                w << it
+                w << '\n'
+            }
+            w.flush()
+        } else if ('/end'.equals(target)) {
+            res.setStatus(200)
+            res.setContentType('text/html')
+            def w = new StringWriter()
+            w << '<!DOCTYPE html>\n'
+            def doc = new MarkupBuilder(w)
+            doc.html(lang : 'ja') {
+                head() {
+                    title('strftime test finish')
+                    meta('http-equiv' : 'Content-Type', content : 'text/html;charset=utf-8')
+                }
+                body() {
+                    h1('server finished')
+                }
+            }
+            res.writer << w.toString()
+            res.writer.flush()
+            server.stop()
+            server.destroy()
+        } else if (js.containsKey(target)) {
+            res.setStatus(200)
+            res.setContentType('text/javascript')
+            def file = js[target]
+            file.eachLine {
+                res.writer << "${it}\n"
+            }
+            res.writer.flush()
+        } else {
+            res.setStatus(404)
+        }
+    }
+}
+
+def handler = new JsUnitTestServer()
+server.setHandler(handler)
+server.start()
---
+++ StrftimeUnitTestFramework/js/strftime_test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: mike
+ * Date: 12/01/03
+ * Time: 1:46
+ * To change this template use File | Settings | File Templates.
+ */
+
+var date = new Date(2009, 11, 5);
+console.log(date.strftime("%Y"));
+console.log(date.strftime("%m"));
+console.log(date.strftime("%d"));
+console.log(date.strftime("%y"));
+console.log(date.strftime("%F"));
